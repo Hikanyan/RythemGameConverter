1c11b378b4bc2c089667706f6a757e46f09a2c20
Hikanyan <hikanyan0131@gmail.com>
2023-05-31 02:19:09 +0900

    変換元のテキストファイルのパスを入力できます。入力後、変換が実行され、参考にしたテキストファイルと同じ場所に新しいJSONファイルが作成されます。

--- RythemGameConverter/RythemGameConverter/Program.cs
+++ RythemGameConverter/RythemGameConverter/Program.cs
@@ -9,28 +9,28 @@ class Program
         Console.WriteLine("変換元のテキストファイルのパスを入力してください:");
         string inputFilePath = Console.ReadLine();   // 変換元のテキストファイルのパス
 
-        Console.WriteLine("変換後のJSONファイルのパスを入力してください:");
-        string outputFilePath = Console.ReadLine();   // 変換後のJSONファイルのパス
-
         try
         {
-            ConvertTextToJson(inputFilePath, outputFilePath);
+            ConvertTextToJson(inputFilePath);
             Console.WriteLine("変換が完了しました。");
         }
         catch (Exception ex)
         {
             Console.WriteLine("変換中にエラーが発生しました:");
             Console.WriteLine(ex.Message);
-        }        
+        }
     }
 
-    static void ConvertTextToJson(string inputFilePath, string outputFilePath)
+    static void ConvertTextToJson(string inputFilePath)
     {
         // テキストファイルを読み込む
         string text = File.ReadAllText(inputFilePath);
 
         // JSONオブジェクトに変換する
         var json = new { text = text };
+
+        // 参考にしたテキストファイルと同じ場所にJSONファイルを作成する
+        string outputFilePath = Path.ChangeExtension(inputFilePath, ".json");
 
         // JSONファイルに書き出す
         string jsonData = JsonConvert.SerializeObject(json);
